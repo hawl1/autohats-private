cace3a66c99afe4df3198b84f1388c2729dc6ecf
Hawli <126889064+hawl1@users.noreply.github.com>
2024-02-10 13:15:32 +0000

    Lint

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
+"""
+    main.py
+
+    Loads cogs and commands
+"""
+
+import os
+
 import discord
 from discord.ext import commands
 from dotenv import dotenv_values
-import os
 
 config = dotenv_values(".env")
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 
 @bot.event
 async def on_ready():
+    """
+    on_ready()
 
-    for filename in os.listdir('./commands'):
-        if filename.endswith('.py'):
-            await bot.load_extension(f'commands.{filename[:-3]}')
+    Loads commands and cogs to bot.
+    """
 
-    for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
-            await bot.load_extension(f'cogs.{filename[:-3]}')
+    for filename in os.listdir("./commands"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"commands.{filename[:-3]}")
 
-    print(f'Logged in as {bot.user.name} ({bot.user.id})')
-    print('------')
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+
+    print(f"Logged in as {bot.user.name} ({bot.user.id})")
+    print("------")
+
 
 bot.run(config["TOKEN"])
